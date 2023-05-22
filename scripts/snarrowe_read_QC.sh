53bb9e9ccf74ccf19077ff57fc738c0e9245f61d
Sarah <sarah.narrowe@scilifelab.se>
2023-05-22 15:47:48 +0200

    assignment 4: finish 3 - Manipulating raw sequencing FASTQ files with seqkit

--- scripts/snarrowe_read_QC.sh
+++ scripts/snarrowe_read_QC.sh
@@ -17,6 +17,38 @@ echo $(gzip -d -c ERR6913103_1.fastq.gz | wc -l)/4|bc
 zgrep "@" ERR6913103_1.fastq.gz | wc -l
 
 # The sequences are in the  Sanger standard format since fastq-dump converts all data to this format according to the linked wikipedia article
+
+# Check if module is available
+module avail | grep "seqkit" 
+module load seqkit
+
+# Print stats on each file 
+cd /shared/home/snarrowe/medbioinfo_folder/sarah/MedBioinfo/data/sra_fastq
+ls |  srun --cpus-per-task=1 --time=00:01:00 xargs seqkit --threads 1 stats
+# Compared that with 
+# sqlite3 -batch /shared/projects/2314_medbioinfo/pascal/central_database/sample_collab.db "select * from sample_annot spl left join sample2bioinformatician s2b using(patient_code) where username='snarrowe'" seems to be a huge difference in the number of reads
+
+# I ran this command to see if any duplicated would be removed and none were, this suggest that duplicates have already been removed
+ls |  srun --cpus-per-task=1 --time=00:01:00 xargs seqkit --threads 1 rmdup >> test.txt
+
+# My guess is that we want to keep duplicates since this could mean having multiple pathogens
+
+# Look for whole adapter 1 
+sra_fastq]$ ls |  srun --cpus-per-task=1 --time=00:01:00 xargs seqkit --threads 1 grep -s -i -p AGATCGGAAGAGCACACGTCTGAACTCCAGTCA >> adapter1.txt
+# Look for whole adapter 2
+ls |  srun --cpus-per-task=1 --time=00:01:00 xargs seqkit --threads 1 grep -s -i -p AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT >> adapter2.txt
+
+# Had to trim the adapter a lot before I got any hits, but I got hits for adapter 2 with this short of an adapter
+ls |  srun --cpus-per-task=1 --time=00:01:00 xargs seqkit --threads 1 grep -s -i -p AGATCG >> adapter2_tiny.txt
+# But when something is trimmed down this much it could also be coincidence
+#Checked how many with the following command 
+cat adapter2_tiny.txt | grep "@" | wc -l # output: 28
+# Doing the same thing for adapter2 gave output 8
+
+# My guess is that they have already been trimmed
+
+# Part 4
+
 date
 echo "script end."
 
